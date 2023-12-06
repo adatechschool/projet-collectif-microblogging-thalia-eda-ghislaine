5501bbfea26fd95e543e409094b4e21bf2087775
Ghislaine Aybram <ghislaine.aybram@gmail.com>
2023-12-06 16:57:00 +0100

    logo coeur et comment

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -1,7 +1,16 @@
 {{-- @extends('layouts.main') --}}
 
 {{-- @include('components.nav-link') --}}
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>Document</title>
+</head>
+<body>
 <h1>Page d'accueil où il y a plein de belles photos</h1>
 
 <ul>
@@ -9,3 +18,6 @@
         <li><a href="{{$post->id}}">{{$post['title']}}</a></li>
     @endforeach
 </ul>
+
+</body>
+</html>
