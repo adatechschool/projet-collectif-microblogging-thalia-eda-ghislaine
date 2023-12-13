de1a606d62133e9384d91323386eb87201a30da7
Eda OZDEMIR <edaozdemir@live.fr>
2023-12-13 15:21:36 +0100

    modif vue mes posts

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -80,182 +80,17 @@
         </div>
       </div>
 
-      <!--status show icon-->
-
-      {{-- <div class="inline-flex ml-36 mt-16">
-        <div class="flex-1 text-center px-4 py-2 m-2">
-          <div
-            class="relative shadow-xl mx-auto h-24 w-24 -my-12 border-white rounded-full overflow-hidden border-4"
-          >
-            <img
-              class="object-cover w-full h-full"
-              src="https://images.unsplash.com/photo-1502164980785-f8aa41d53611?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80"
-            />
-          </div>
-          <h1 class="pt-16 text-base font-semibold text-gray-900">Fun</h1>
-        </div>
-
-        <div class="flex-1 text-center px-4 py-2 m-2">
-          <div
-            class="relative shadow-xl mx-auto h-24 w-24 -my-12 border-white rounded-full overflow-hidden border-4"
-          >
-            <img
-              class="object-cover w-full h-full"
-              src="https://images.unsplash.com/photo-1456415333674-42b11b9f5b7b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80"
-            />
-          </div>
-          <h1 class="pt-16 text-base font-semibold text-gray-900">Travel</h1>
-        </div>
-
-        <div class="flex-1 text-center px-4 py-2 m-2">
-          <div
-            class="relative shadow-xl mx-auto h-24 w-24 -my-12 border-white rounded-full overflow-hidden border-4"
-          >
-            <img
-              class="object-cover w-full h-full"
-              src="https://images.unsplash.com/photo-1494972308805-463bc619d34e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1052&q=80"
-            />
-          </div>
-          <h1 class="pt-16 text-base font-semibold text-gray-900">Food</h1>
-        </div>
-
-        <div class="flex-1 text-center px-4 py-2 m-2">
-          <div
-            class="relative shadow-xl mx-auto h-24 w-24 -my-12 border-white rounded-full overflow-hidden border-4"
-          >
-            <img
-              class="object-cover w-full h-full"
-              src="https://images.unsplash.com/photo-1516834474-48c0abc2a902?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1053&q=80"
-            />
-          </div>
-          <h1 class="pt-16 text-base font-semibold text-gray-900">Sketch</h1>
-        </div>
-
-        <div class="flex-1 text-center px-4 py-2 m-2">
-          <div
-            class="relative shadow-xl mx-auto h-24 w-24 -my-12 border-white rounded-full overflow-hidden border-4"
-          >
-            <img
-              class="object-cover w-full h-full"
-              src="https://images.unsplash.com/photo-1444021465936-c6ca81d39b84?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80"
-            />
-          </div>
-          <h1 class="pt-16 text-base font-semibold text-gray-900">My Work</h1>
-        </div>
-      </div> --}}
+     
 
       <hr class="border-gray-500 mt-6" />
       <hr class="border-gray-500 w-20 border-t-1 ml-64 border-gray-800" />
 
-      <!--post icon and title-->
-      {{-- <div class="flex flex-row mt-4 justify-center mr-16">
-        <div class="flex text-gray-700 text-center py-2 m-2 pr-5">
-          <div class="flex inline-flex">
-            <button
-              class="border-transparent text-gray-800 rounded-full hover:text-blue-600 focus:outline-none focus:text-gray-600"
-              aria-label="Notifications"
-            >
-              <svg
-                class="h-6 w-6"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  d="M17 14v6m-3-3h6M6 10h2a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2zm10 0h2a2 2 0 002-2V6a2 2 0 00-2-2h-2a2 2 0 00-2 2v2a2 2 0 002 2zM6 20h2a2 2 0 002-2v-2a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2z"
-                />
-              </svg>
-            </button>
-          </div>
-          <div class="flex inline-flex ml-2 mt-1">
-            <h3 class="text-sm font-bold text-gray-800 mr-2">POSTS</h3>
-          </div>
-        </div>
-
-        <div class="flex text-gray-700 text-center py-2 m-2 pr-5">
-          <div class="flex inline-flex">
-            <button
-              class="border-transparent text-gray-600 rounded-full hover:text-blue-600 focus:outline-none focus:text-gray-600"
-              aria-label="Notifications"
-            >
-              <svg
-                class="h-6 w-6"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-                />
-              </svg>
-            </button>
-          </div>
-          <div class="flex inline-flex ml-2 mt-1">
-            <h3 class="text-sm font-medium text-gray-700 mr-2">IGTV</h3>
-          </div>
-        </div>
-
-        <div class="flex text-gray-700 text-center py-2 m-2 pr-5">
-          <div class="flex inline-flex">
-            <button
-              class="border-transparent text-gray-600 rounded-full hover:text-blue-600 focus:outline-none focus:text-gray-600"
-              aria-label="Notifications"
-            >
-              <svg
-                class="h-6 w-6"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-              >
-                <path d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" />
-              </svg>
-            </button>
-          </div>
-          <div class="flex inline-flex ml-2 mt-1">
-            <h3 class="text-sm font-medium text-gray-700 mr-2">SAVED</h3>
-          </div>
-        </div>
-
-        <div class="flex text-gray-700 text-center py-2 m-2 pr-5">
-          <div class="flex inline-flex">
-            <button
-              class="border-transparent text-gray-600 rounded-full hover:text-blue-600 focus:outline-none focus:text-gray-600"
-              aria-label="Notifications"
-            >
-              <svg
-                class="h-6 w-6"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z"
-                />
-              </svg>
-            </button>
-          </div>
-          <div class="flex inline-flex ml-2 mt-1">
-            <h3 class="text-sm font-medium text-gray-700 mr-2">TAGGED</h3>
-          </div>
-        </div>
-      </div> --}}
+     
      
       
       </a>
 
-      <div class="grid grid-cols-2 gap-4">
+      {{-- <div class="grid grid-cols-2 gap-4">
         @forelse ($posts as $post)
             <div class="flex-1 text-center px-4 py-2 m-2">
                 <p class="mb-2">{{ $post->title }}</p>
@@ -271,22 +106,69 @@
                 
                 </div>
     
-                <form action="{{ route('deletePost', $post->id) }}" method="POST">
-                    @csrf
-                    @method('DELETE')
-    
-                    <button type="submit" class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded">
-                      Supprimer
-                    </button>
-                </form>
+              
             </div>
         @empty
             <p>Aucun post disponible pour le moment.</p>
-        @endforelse
+        @endforelse --}}
     </div>
     
-    
-    
+    <div class="max-w-xl mx-auto mt-10">
+      <div class="grid grid-cols-1 gap-6">  
+        
+        @foreach($posts as $post)
+          <div class="bg-gray-100 p-4">
+  
+            <div class="bg-white border rounded-sm max-w-md p-4">
+              
+              <div class="flex items-center mb-4">
+               
+                <img class="h-6 w-6 rounded-full" src="https://picsum.photos/id/1027/150/150"/>
+                <div class="ml-3">
+                  <span class="text-lg font-semibold antialiased block leading-tight">
+                    <a href="{{ route('post.show', $post->id) }}">{{$post['title']}}
+                      <span class="text-xs text-gray-600 block">
+                        Publié le {{ $post->created_at }} par : 
+                        <a href="{{ route('users.show', $post->user_id) }}" class="underline">{{ \App\Models\User::find($post->user_id)->name }}</a>
+                      </span>
+                    </div>
+                  </div>
+              
+                  <div class="relative">
+                    @if($post->image)
+                      <img src="{{ asset('storage/' . $post->image) }}" alt="Image du post" class="w-full h-40 object-cover mb-4">
+                    @else
+                      <p class="w-full h-40 flex items-center justify-center bg-gray-200 text-gray-500 mb-4">Aucune image</p>
+                    @endif
+              
+                    <p class="text-gray-600 mb-4 overflow-hidden">
+                      {{ \Illuminate\Support\Str::limit($post->content, 100, $end='...') }}
+                      @if (strlen($post->content) > 100)
+                        <a href="{{ route('post.show', $post->id) }}" class="more-link">Lire plus</a>
+                      @endif
+                    </p>
+                  </div>
+              
+                <div class="flex items-center justify-between mx-4 mt-3">
+                <div class="flex gap-3 items-center">
+                  {{ $post->likes->count() }}<svg fill="#262626" height="20" viewBox="0 0 48 48" width="24"><path d="M34.6 6.1c5.7 0 10.4 5.2 10.4 11.5 0 6.8-5.9 11-11.5 16S25 41.3 24 41.9c-1.1-.7-4.7-4-9.5-8.3-5.7-5-11.5-9.2-11.5-16C3 11.3 7.7 6.1 13.4 6.1c4.2 0 6.5 2 8.1 4.3 1.9 2.6 2.2 3.9 2.5 3.9.3 0 .6-1.3 2.5-3.9 1.6-2.3 3.9-4.3 8.1-4.3m0-3c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5.6 0 1.1-.2 1.6-.5 1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path></svg>
+                  <svg fill="#262626" height="20" viewBox="0 0 48 48" width="24"><path clip-rule="evenodd" d="M47.5 46.1l-2.8-11c1.8-3.3 2.8-7.1 2.8-11.1C47.5 11 37 .5 24 .5S.5 11 .5 24 11 47.5 24 47.5c4 0 7.8-1 11.1-2.8l11 2.8c.8.2 1.6-.6 1.4-1.4zm-3-22.1c0 4-1 7-2.6 10-.2.4-.3.9-.2 1.4l2.1 8.4-8.3-2.1c-.5-.1-1-.1-1.4.2-1.8 1-5.2 2.6-10 2.6-11.4 0-20.6-9.2-20.6-20.5S12.7 3.5 24 3.5 44.5 12.7 44.5 24z" fill-rule="evenodd"></path></svg>
+                  <svg fill="#262626" height="20" viewBox="0 0 48 48" width="24"><path d="M47.8 3.8c-.3-.5-.8-.8-1.3-.8h-45C.9 3.1.3 3.5.1 4S0 5.2.4 5.7l15.9 15.6 5.5 22.6c.1.6.6 1 1.2 1.1h.2c.5 0 1-.3 1.3-.7l23.2-39c.4-.4.4-1 .1-1.5zM5.2 6.1h35.5L18 18.7 5.2 6.1zm18.7 33.6l-4.4-18.4L42.4 8.6 23.9 39.7z"></path></svg>
+                </div>
+                <form action="{{ route('deletePost', $post->id) }}" method="POST">
+                  @csrf
+                  @method('DELETE')
+  
+                  <button type="submit" class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded">
+                    Supprimer
+                  </button>
+              </form>
+              </div>
+            </div>
+          </div>
+        @endforeach
+      </div>
+    </div> 
   
 
 
